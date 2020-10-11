d9ff5d52ae7c405e7518f859dec16ba6b0458ffb
spkell21 <skelly240@gmail.com>
2020-10-11 18:04:31 -0400

    ts_class_varTrials stores all performed classifications in a structure
    I'm about to run tEEG_ts_class_varTrials_v1 for all subjects and all trials... Hope I dont need to bring my laptop anywhere

--- tEEG_ts_class_varTrials_v1.m
+++ tEEG_ts_class_varTrials_v1.m
@@ -1,3 +1,4 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Author: Sean Kelly
 %Filename: tEEG_ts_class_varTrials_v1.m
 %Date: 10/11/20
@@ -20,67 +21,61 @@
 %Dependencies: FieldTrip, CosmoMVPA
 %
 %Example: tEEG_ts_class_varTrials
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Classifier conditions
 fix_pos = 1;
-ntrials = 100;
+%ntrials = 100; %all trials
+ntrials = 10; %Debug with small number of trials
 
 % reset citation list
 cosmo_check_external('-tic');
 
 time_values = (1:494); % first dim (channels got nuked)
-%nsubjects = 10; %all subjects
-nsubjects = 1; %debug with only 1 subject to reduce time complexity by 10x
-
-%Preallocate memory to store roc for both tEEG and eEEG
-roc(1:2,ntrials-2) = zeros(2,1); %2eeg_types * 98trials
-
-%Preallocate memory of null matrix to reinitialize class_raw_mat
-class_raw_mat_zeros(:,size(time_values,2)) = zeros(nsubjects,1); %10subjects x 494ms
+nsubjects = 10; %all subjects
+%nsubjects = 1; %DEBUG: only 1 subject to reduce time complexity by 10x
 
 %Preallocate memory of matrix to store all classification scores generated
-%ts_class_mat(1:nsubjects,size(time_values,2),ntrials-2,2) = zeros(1,1); %10subjects x 494ms x 98trials x 2eegs
+ts_class_mat(1:nsubjects,size(time_values,2),ntrials-2,2) = zeros(); %10subjects x 494ms x 98trials x 2eegs
 
-%Extract classifier scored for 
+%Extract classifier scored for each trial combination
 for eeg_type=1:2 %tEEG and eEEG
 
     for trial_count=1:ntrials-2
-
-        %Preallocate memory to store classification of each subject
-        class_raw_mat = class_raw_mat_zeros; %10subjects x 494cl_p
 
         %Runs timeseries classification for each subject
         for subject=1:nsubjects
 
             %runs ts classification
-            sample_map = tEEG_ts_class_backend(subject, fix_pos, eeg_type, trial_count+2); %10subjects x 494(classifier performance)
+            sample_map = tEEG_ts_class_backend(subject, fix_pos, eeg_type, trial_count+2); %1subject x 494(classifier performance)
             %trial_count+2 since 1,2 trials aren't accepted by classifier
             
-            class_raw_mat(subject,:) = sample_map; %10subjects x 494(classifier performance)
-            %ts_class_mat(subject,:,trial_count,eeg_type) = sample_map; %10subjects x 494cl-performance x 98trials x 2eegs
-            
+            ts_class_mat(subject,:,trial_count,eeg_type) = sample_map; %10subjects x 494cl-performance x 98trials x 2eegs
         end
-        
-        %Average classification accuracy across all subjects for each timepoint
-        %class_avg = mean(class_raw_mat,1); %1 x 494cl-performance
-        %roc(eeg_type,trial_count) = integral_sl_map(time_values,class_avg)
-        
     end
 end
 
 %{
+%DEBUG: 1 subject, makes all subject's data match subject 1
+for subject=2:nsubjects
+    ts_class_mat(subject,:,:,:) = ts_class_mat(1,:,:,:);
+end
+%}
 
 %Average classification accuracy across all subjects for each timepoint
-class_avg = mean(ts_class_mat(:,:,:,:),1); %1 x 494cl-performance x 98trials x 2eegs
+class_avg = mean(ts_class_mat,1); %1 x 494cl-performance x 98trials x 2eegs
+class_avg = squeeze(class_avg); %494cl-performance x 98trials x 2eegs
+
+%Preallocate memory to store roc for both tEEG and eEEG
+roc(1:2,ntrials-2) = zeros(); %2eeg_types * 98trials
 
 %calculate integral of each average classification vector
 for eeg_type=1:2 %tEEG and eEEG
     for trial_count=1:ntrials-2
         %store area under curve for each classification plot for each 
-        roc(eeg_type,trial_count) = integral_sl_map(time_values,class_avg(1,:,trial_count,eeg_type)); %2eeg x 98trials
+        roc(eeg_type,trial_count) = integral_sl_map(time_values,class_avg(:,trial_count,eeg_type)); %2eeg x 98trials
     end
 end
-%}
 
 figure; %figure to display roc of both eeg types
 
@@ -91,23 +86,45 @@ for eeg_type=1:2
     hold on
 end
 
-%Configure individual classification plot
+%Configure average classification plot
 xlim([min(3),max(ntrials)]);
 ylabel('Area Under Curve (Class Accuracy * ms)');
 xlabel('Trials Used to Train Classifier');
-title('ROC Curve: tEEG vs eEEG');
+title('Average ROC Curve: tEEG vs eEEG');
 legend('tEEG','eEEG');
 MarkPlot('Fix_pos:center');
-
-%{
+hline(0,'k','chance');
 
 %calculate integral of each subject's classification vector
-roc_indiv(1:10,1:2,ntrials-2) = zeros(2,1); %10subjects * 2eeg_types * 98trials
-for subject=1:nsubjects
-    
+roc_indiv(1:nsubjects,1:2,ntrials-2) = zeros(); %10subjects * 2eeg_types * 98trials
+for eeg_type=1:2
+    for subject=1:nsubjects
+        for trial_count=1:ntrials-2
+            roc_indiv(subject,eeg_type,trial_count) = integral_sl_map(time_values,ts_class_mat(subject,:,trial_count,eeg_type));
+        end
+    end
 end
 
-%Plot each subject's tEEG vs eEEG ROC curve
-
-%}
-
+%retrieve participant conditions
+conditions = tEEG_conditions();
+%Plot each subject's tEEG vs eEEG ROC curve separately
+figure;
+for subject=1:nsubjects
+    subplot(ceil(nsubjects/2),2,subject);
+    for eeg_type=1:2
+        roc_vector = squeeze(roc_indiv(subject,eeg_type,:));
+        plot(1:ntrials-2,roc_vector);
+        hold on
+    end
+    
+    %Configure individual classification plot
+    xlim([min(3),max(ntrials)]);
+    ylabel('Classifier Integral'); %Class Accuracy * ms
+    xlabel('Trials Used to Train Classifier');
+    title_string = strcat('ROC: tEEG vs eEEG // Subject: ', conditions{1}{subject});
+    title(title_string);
+    legend('tEEG','eEEG');
+    hline(0,'k','chance');
+    hold off
+end
+MarkPlot('Fix_pos:center');
