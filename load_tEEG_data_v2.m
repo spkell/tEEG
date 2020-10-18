2b917c891e9b07a682489e6ac677333ee17a888d
spkell21 <skelly240@gmail.com>
2020-10-18 13:23:54 -0400

    Update load_tEEG_data_v2.m

--- load_tEEG_data_v2.m
+++ load_tEEG_data_v2.m
@@ -16,7 +16,7 @@ function data = load_tEEG_data_v2(subject, fixation_pos, stim)
     %Sets path for tEEG data
     %Change depending on desired path
     directory_path = '~/Documents/GitHub/tEEG/tEEG_checkerboard_data';
-    %directory_path = '~/VNL/projects/tEEG/DataMat'; %dir path in lab
+    %directory_path = '~/VNL/Projects/tEEG/DataMat'; %dir path in lab
 
     %Build requested file name in correct path
     fname = ['tEEG_' subject '_' stim '_' fixation_pos '.mat']; % filename (no path)
@@ -25,7 +25,7 @@ function data = load_tEEG_data_v2(subject, fixation_pos, stim)
     %checks if parameters match a given file
     if exist(fn,'file')
         fprintf('loading %s...\n',fname);
-        simian = load(fname); %file exists, load file
+        simian = load(fn); %file exists, load file
         data = simian.simian;
         fprintf('\tn timepoints = %d\n',size(data.data,2)) %trial info
         fprintf('\tn trials     = %d\n',size(data.data,3))
