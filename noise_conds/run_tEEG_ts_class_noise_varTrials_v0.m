7db115e362f8cecbcf50bffc44d1b57b27a33e9d
spkell21 <skelly240@gmail.com>
2021-03-21 19:18:53 -0400

    Var Trials auc noise conds

--- noise_conds/run_tEEG_ts_class_noise_varTrials_v0.m
+++ noise_conds/run_tEEG_ts_class_noise_varTrials_v0.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Author: Sean Kelly
 %Filename: run_tEEG_ts_class_noise_varTrials_v0.m
-%Date: 12/29/20
+%Date: 3/20/21
 %
 %Purpose: This classifier performs MVPA analyses on tEEG and eEEG data.
 % 1 plot is produced containing a superposition of all 10 subjects, and
@@ -23,8 +23,9 @@
 %Classifier conditions
 noises = {'Center','Clench','Chew'};
 noise_pos = 2; %[Center,Clench,Chew]
-eeg_type = [1,2];
+eeg_types = [1,2];
 parietal = 1; %Include parietal electrode channels
+repetitions = 10; %More accurate representation of clssifier abilities
 
 % sample attributes
 nsamp = 9; %all subjects but first, since they have no noise conditions
@@ -34,7 +35,7 @@ time_values = (1:nfeat); % first dim (channels got nuked)
 targets = ones(nsamp,1);
 chunks = (1:nsamp)';
 
-ntarget_combinations = length(eeg_type);
+ntarget_combinations = length(eeg_types);
 chance = 1 / ntarget_combinations;
 
 ntrials = 100; %all trials
@@ -44,51 +45,84 @@ trial_sel = valid_trials(2:ntrial_steps:end); %selected ntrial lengths to examin
 len_trial_sel = length(trial_sel);
 
 %Preallocate memory of matrix to store all classification scores generated
-ts_class_mat(1:nsamp,nfeat,len_trial_sel,length(eeg_type)) = zeros(); %9subjects x 494ms x ntrial_sizes x n_eegs
+total_class_mat = zeros(nsamp,nfeat,len_trial_sel,length(eeg_types)); %9subjects x 494ms x ntrial_sizes x n_eegs
 
-%Preallocate memory to store classification of each subject
-class_raw_mat(length(eeg_type),nsamp,nfeat) = zeros();
-
-for eeg=1:length(eeg_type)
+for eeg=1:length(eeg_types)
     for trial_count=1:len_trial_sel
     
         %Runs timeseries classification for each subject
         for subject=1:nsamp
-
-            %runs ts classification
-            %sample_map = tEEG_ts_class_backend_noise_v1(subject+1, noise_pos, eeg, ntrials, parietal); %1class-score x 494timepoints
-            sample_map = tEEG_ts_class_backend_noise_v2(subject+1, noise_pos, eeg, ntrials, parietal); %1class-score x 494timepoints
-
-            ts_class_mat(subject,:,trial_count,eeg) = sample_map; %9subjects x 494cl-performance x ntrials_selected x 2eegs
+            
+            sample_sum = zeros(1,nfeat);
+            for rep=1:repetitions
+                
+                %runs ts classification
+                sample_map = tEEG_ts_class_backend_noise_v2(subject+1, noise_pos, eeg, ntrials, parietal); %1class-score x 494timepoints
+                sample_sum = sample_sum + sample_map;
+            end
+            
+            %store classification results
+            total_class_mat(subject,:,trial_count,eeg) = sample_sum/repetitions; %9subjects x 494cl-performance x ntrials_selected x 2eegs
         end  
     end
 end
 
 
-%calculate integral of each subject's classification vector
-roc_indiv_zeros(1:nsamp,length(eeg_type),len_trial_sel) = zeros(); %9subjects * n_eeg_types * ntrials_selected
-roc_indiv = roc_indiv_zeros;
-for eeg=1:length(eeg_type)
+%calculate average of each subject's classification vector
+auc_indiv = zeros(nsamp,length(eeg_types),len_trial_sel); %9subjects * n_eeg_types * ntrials_selected
+for eeg=1:length(eeg_types)
     for subject=1:nsamp
         for trial_count=1:len_trial_sel
-            roc_indiv(subject,eeg,trial_count) = integral_sl_map(time_values,ts_class_mat(subject,:,trial_count,eeg));
+            auc_indiv(subject,eeg,trial_count) = mean(total_class_mat(subject,:,trial_count,eeg));
         end
     end
 end
 
 
+%Find 95% confidence interval for mean of all subjects AUC x ntrials
+%using each participants independent AUC's, for both tEEG and eEEG
+confidence_interval_eeg(2,len_trial_sel) = zeros();
+for eeg=1:size(eeg_types,2)
+    temp_conf_int = ci(auc_indiv(:,eeg,:),95,1);
+    confidence_interval_eeg(eeg,:) = squeeze(temp_conf_int)';
+end
+
+
 %Average classification accuracy across all subjects for each timepoint
-class_avg = mean(ts_class_mat,1); %1 x 494cl-performance x 98trials x 2eegs
+class_avg = mean(total_class_mat,1); %1 x 494cl-performance x 98trials x 2eegs
 class_avg = squeeze(class_avg); %494cl-performance x 98trials x 2eegs
 
 %Preallocate memory to store roc for both tEEG and eEEG
-roc_zeros(1:2,len_trial_sel) = zeros(); %2eeg_types * 98trials
-roc = roc_zeros; %resets size in case of larger roc matrix loaded
+auc = zeros(length(eeg_types),len_trial_sel);  %2eeg_types * 98trials
 
 %calculate integral of each average classification vector
-for eeg=1:length(eeg_type) %tEEG and eEEG
+for eeg=1:length(eeg_types) %tEEG and eEEG
     for trial_count=1:len_trial_sel
         %store area under curve for each classification plot for each 
-        roc(eeg,trial_count) = integral_sl_map(time_values,class_avg(:,trial_count,eeg)); %2eeg x 98trials
+        auc(eeg,trial_count) = mean(class_avg(:,trial_count,eeg)); %2eeg x 98trials
     end
 end
+
+%Plot average area under curve for tEEG and eEEG
+chance = 0;
+color = 'b';
+for eeg=1:size(eeg_types,2)
+    if eeg == 2
+        color = 'r';
+    elseif eeg == 3
+        color = 'g';
+    end
+    continuous_error_bars(auc(eeg,:), trial_sel, confidence_interval_eeg(eeg,:), 0, color, 0)
+    hold on
+end
+color = 'b';
+for eeg=1:size(eeg_types,2)
+    if eeg == 2
+        color = 'r';
+    elseif eeg == 3
+        color = 'g';
+    end
+    plot(trial_sel, auc(eeg,:), color, 'LineWidth', 3)
+end
+
+
